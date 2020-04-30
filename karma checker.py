9b517ed8be141e467b223ce9a0eb12cf7c53e7a9
pixelmaker06 <pixelmaker06@gmail.com>
2020-04-30 16:36:34 +0930

    updated and made it better

--- karma checker.py
+++ karma checker.py
@@ -4,9 +4,11 @@ try:
 except:
   os.system('py -m pip install selenium')
 
+username = input("Username \n> ")
+
 driver = webdriver.Chrome("drivers\chromedriver.exe ")
 
 driver.set_page_load_timeout(10)
-driver.get("https://www.reddit.com/user/{}/".format(input("Username \n> ")))
+driver.get("https://www.reddit.com/user/{}/".format(username))
 print(driver.find_element_by_id("profile--id-card--highlight-tooltip--karma").get_attribute('innerHTML'))
 driver.quit()
