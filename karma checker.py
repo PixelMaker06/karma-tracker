ac8c60bfb097b03bd6a875b9fce8166f835089b6
PixelMaker06 <57247369+PixelMaker06@users.noreply.github.com>
2020-04-30 16:29:18 +0930

    Update karma checker.py

--- karma checker.py
+++ karma checker.py
@@ -1,10 +1,12 @@
 import os
-os.system('py -m pip install selenium')
-from selenium import webdriver
+try:
+  from selenium import webdriver
+except:
+  os.system('py -m pip install selenium')
 
 driver = webdriver.Chrome("drivers\chromedriver.exe ")
 
 driver.set_page_load_timeout(10)
-driver.get("https://www.reddit.com/user/PixelMaker06/")
+driver.get("https://www.reddit.com/user/{}/".format(input("Username \n> ")))
 print(driver.find_element_by_id("profile--id-card--highlight-tooltip--karma").get_attribute('innerHTML'))
 driver.quit()
